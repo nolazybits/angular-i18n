06b241266766126dcdf20315b441ac3ab6f6033f
nolazybits <nolazybits@gmail.com>
2015-06-17 17:22:13 +1200

    - code coverage and badges

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-angular-i18n is a small Angular module in charge of i18n.  
-[![wercker status](https://app.wercker.com/status/4c29fbca7d6e75236843bdb3752f2273/m "wercker status")](https://app.wercker.com/project/bykey/4c29fbca7d6e75236843bdb3752f2273)
+[![NPM](https://nodei.co/npm/angular-internationalization.png)](https://nodei.co/npm/angular-internationalization/)  
+[![wercker status](https://app.wercker.com/status/4c29fbca7d6e75236843bdb3752f2273/s "wercker status")](https://app.wercker.com/project/bykey/4c29fbca7d6e75236843bdb3752f2273)    
 
+angular-i18n is a small Angular module in charge of i18n.  
 
 ### Dependencies
 
--- karma.conf.js
+++ karma.conf.js
@@ -37,14 +37,22 @@ module.exports = function (config) {
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
+        preprocessors: {
+            'angular-internationalization.js': 'coverage'
+        },
 
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress', 'html'],
+        reporters: ['progress', 'html', 'coverage'],
 
+        coverageReporter: {
+            reporters:[
+                {type: 'html'},
+                {type: 'cobertura', subdir: '.'}
+            ]
+        },
 
         // web server port
         port: 9876,
